218682a4ca762455bb558e023d2f58837a99eb85
bgt98 <58718052+bgt98@users.noreply.github.com>
2019-12-10 07:14:17 -0500

    Update prioridadeescrita.java

--- prioridadeescrita.java
+++ prioridadeescrita.java
@@ -2,44 +2,44 @@
 (leitores nao podem iniciar nova leitura quando h  ̃ a escritores esperando para escrever).*/
 
 class LE {
-private int leit, escr, escresp;
-LE() {
-this.leit = 0; //numero de leitores lendo
-this.escr = 0; //numero de escritores escrevendo (0 ou 1)
-this.escresp = 0; //numero de escritores esperando
-}
-// Entrada para leitores
+  private int leit, escr, escresp;
+  LE() {
+    this.leit = 0; //numero de leitores lendo
+    this.escr = 0; //numero de escritores escrevendo (0 ou 1)
+    this.escresp = 0; //numero de escritores esperando
+  }
+  // Entrada para leitores
 
-public synchronized void EntraLeitor (int id) {
-try {
-while (this.escr > 0 || this.escresp > 0) {
-wait(); //bloqueia se houver escritores esperando ou escrevendo
-}
-this.leit++;
-} catch (InterruptedException e) { }
-}
-// Saida para leitores
-public synchronized void SaiLeitor (int id) {
-this.leit--;
-if (this.leit == 0)
-notifyAll(); //libera os escritores
-//(a fila da variavel de condicao pode ter leitores e escritores)
+  public synchronized void EntraLeitor (int id) {
+    try {
+      while (this.escr > 0 || this.escresp > 0) {
+      wait(); //bloqueia se houver escritores esperando ou escrevendo
+      }
+      this.leit++;
+    } catch (InterruptedException e) { }
+  }
+  // Saida para leitores
+  public synchronized void SaiLeitor (int id) {
+    this.leit--;
+    if (this.leit == 0)
+    notifyAll(); //libera os escritores
+    //(a fila da variavel de condicao pode ter leitores e escritores)
 
-}
-// Entrada para escritores
-public synchronized void EntraEscritor (int id) {
-try {
-this.escresp++; //sinaliza que quer escrever
-while ((this.leit > 0) || (this.escr > 0)) {
-wait(); //bloqueia se houver leitores lendo ou outro escritor escrevendo
-}
-this.escresp--;
-this.escr++;
-} catch (InterruptedException e) { }
-}
-// Saida para escritores
-public synchronized void SaiEscritor (int id) {
-this.escr--;
-notifyAll(); //libera os leitores e outros escritores
-}
+  }
+  // Entrada para escritores
+  public synchronized void EntraEscritor (int id) {
+    try {
+      this.escresp++; //sinaliza que quer escrever
+      while ((this.leit > 0) || (this.escr > 0)) {
+        wait(); //bloqueia se houver leitores lendo ou outro escritor escrevendo
+      }
+      this.escresp--;
+      this.escr++;
+    } catch (InterruptedException e) { }
+  }
+  // Saida para escritores
+  public synchronized void SaiEscritor (int id) {
+    this.escr--;
+    notifyAll(); //libera os leitores e outros escritores
+  }
 }
