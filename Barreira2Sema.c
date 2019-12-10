e18e6b0db7606cf7edcd2f848bd4e08ab1ecd6e6
bgt98 <58718052+bgt98@users.noreply.github.com>
2019-12-10 08:29:42 -0500

    Update Barreira2Sema.c

--- Barreira2Sema.c
+++ Barreira2Sema.c
@@ -8,23 +8,23 @@ sem_t em, par; //em: exclus ̃ao m ́utua; par: barreira (condicional)
 int cont=0; //contador de threads para a barreira
 sem_init(&em, 0, 1); sem_init(&par,0,0); //globais
 proc T() {
-while(true) {
-//executa c ́odigo que n ̃ao depende do recurso...
-sem_wait(&em); //exclus ̃ao m ́utua para acesso a vari ́aveis compartilhadas
-cont++; //incrementa contador de threads que querem usar o recurso
-if(cont!=2) {//ainda n ̃ao existe um par
-sem_post(&em); //libera a exclus ̃ao m ́utua
-sem_wait(&par); //bloqueia para esperar outra thread
-sem_post(&par); //deixa um sinal no sem ́aforo para us ́a-lo comm EM adiante
-} else { //j ́a existe um par
-sem_post(&par); //libera a outra thread e mantem a exclus ̃ao m ́utua
-}
-//executa o c ́odigo que usa o recurso...
-sem_wait(&par); //usa o semaforo ’par’ para EM entre as duas threads
-cont--; //terminou de usar o recurso
-if(cont==0) //as duas threads terminaram de usar o recurso, libera a EM para as demais
-sem_post(&em);
-else
-sem_post(&par); //usa o semaforo ’par’ para EM entre as duas threads
-}
+  while(true) {
+    //executa c ́odigo que n ̃ao depende do recurso...
+    sem_wait(&em); //exclus ̃ao m ́utua para acesso a vari ́aveis compartilhadas
+    cont++; //incrementa contador de threads que querem usar o recurso
+    if(cont!=2) {//ainda n ̃ao existe um par
+      sem_post(&em); //libera a exclus ̃ao m ́utua
+      sem_wait(&par); //bloqueia para esperar outra thread
+      sem_post(&par); //deixa um sinal no sem ́aforo para us ́a-lo comm EM adiante
+    } else { //j ́a existe um par
+      sem_post(&par); //libera a outra thread e mantem a exclus ̃ao m ́utua
+    }
+    //executa o c ́odigo que usa o recurso...
+    sem_wait(&par); //usa o semaforo ’par’ para EM entre as duas threads
+    cont--; //terminou de usar o recurso
+    if(cont==0) //as duas threads terminaram de usar o recurso, libera a EM para as demais
+      sem_post(&em);
+    else
+      sem_post(&par); //usa o semaforo ’par’ para EM entre as duas threads
+  }
 }
