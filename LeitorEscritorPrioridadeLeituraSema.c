bc42970c669ff1576c674bc4164d00201d868e0a
bgt98 <58718052+bgt98@users.noreply.github.com>
2019-12-10 08:33:20 -0500

    Update LeitorEscritorPrioridadeLeituraSema.c

--- LeitorEscritorPrioridadeLeituraSema.c
+++ LeitorEscritorPrioridadeLeituraSema.c
@@ -3,39 +3,42 @@ ma dos leitores e escritores (com prioridade para leitura). As condições do pr
 
 (i) os leitores podem ler simultaneamente a região de dados compartilhada; (ii) apenas
 um escritor pode escrever a cada instante na região de dados compartilhada. (iii) se um
-escritor está escrevendo, nenhum leitor pode ler a mesma região de dados compartilhada.
+escritor está escrevendo, nenhum leitor pode ler a mesma região de dados compartilhada.*/
+
+
 int num_leitores; //registra o numero de leitores ativos
-sem em=1; //semaforo binario para exclusao mutua entre*/
+sem em=1; //semaforo binario para exclusao mutua entre
 
 //leitores para atualizacao de variaveis compartilhadas
 sem wsem=1; //semaforo binario para exclusao mutua para escrita
 void leitor() {
-while(true) {
-semWait(em);
-num_leitores++;
-//o primeiro leitor verifica ausencia de escritores
-if(num_leitores==1) {
-semWait(wsem);
-}
-semSignal(em);
-le_dados();
-semWait(em);
-num_leitores--;
-if(num_leitores==0) { //o ultimo leitor libera a escrita
-semSignal(wsem);
-}
-semSignal(em);
-}
+  while(true) {
+    semWait(em);
+    num_leitores++;
+    //o primeiro leitor verifica ausencia de escritores
+    if(num_leitores==1) {
+      semWait(wsem);
+    }
+    semSignal(em);
+    le_dados();
+    semWait(em);
+    num_leitores--;
+    if(num_leitores==0) { //o ultimo leitor libera a escrita
+      semSignal(wsem);
+    }
+    semSignal(em);
+  }
 }
 
 void escritor() {
-while(true) {
-semWait(wsem);
-escreve_dados();
-semSignal(wsem);
-}
+  while(true) {
+    semWait(wsem);
+    escreve_dados();
+    semSignal(wsem);
+  }
 }
 void main() {
-num_leitores=0;
-cria_thread(leitor); //cria as threads leitoras
-cria_thread(escritor); //cria as threads escritoras
+  num_leitores=0;
+  cria_thread(leitor); //cria as threads leitoras
+  cria_thread(escritor); //cria as threads escritoras
+}
